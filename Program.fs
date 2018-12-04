06b63a43558246e57340289e5ea7482fe0a404be
Sergei Bolotov <sbolotov@microsoft.com>
2018-12-03 16:10:29 -0800

    Добавьте файлы проекта.

---
+++ Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+namespace Advent2018
+
+open Task1
+
+module Main =
+
+    [<EntryPoint>]
+    let main argv = 
+        let file = new System.IO.StreamReader("../../Tasks/input1.txt")
+        printfn "%A" <| task1and1 file
+        System.Console.ReadKey() |> ignore
+        0 // return an integer exit code
---
+++ Task1.fs
@@ -0,0 +1,26 @@
+﻿namespace Advent2018
+
+module Task1 =
+    let getNumbers(file : System.IO.StreamReader) =
+        let str = file.ReadToEnd()
+        let numbers = str.Split('\n')
+        Array.filter (fun (x : string) -> x.Length > 0) numbers
+            |> Array.map (fun (x : string) -> (if x.[0].Equals('+') then x.Substring(1) else x) |> int)
+
+
+    let task1 (file : System.IO.StreamReader) =
+            getNumbers(file) |> Array.sum
+
+    let task1and1 (file : System.IO.StreamReader) =
+        let numbers = getNumbers(file)
+        let history = new System.Collections.Generic.HashSet<int>()
+        let result = new System.Collections.Generic.HashSet<int>()
+
+        let rec fold (acc : int) = 
+            let x = Array.fold (fun acc elem ->
+                        if history.Contains(acc) then printfn "%A" acc; result.Add acc |> ignore
+                        else history.Add(acc) |> ignore
+                        acc + elem) acc numbers
+            if(result.Count = 0) then fold(x) |> ignore
+
+        fold(0)
