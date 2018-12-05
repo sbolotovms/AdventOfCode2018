f026e2c1401b08dd151cc3037f83c35b411acde9
Sergei Bolotov <sbolotov@microsoft.com>
2018-12-05 10:41:41 -0800

    First step in task 3

--- Program.fs
+++ Program.fs
@@ -2,13 +2,10 @@
 // See the 'F# Tutorial' project for more help.
 namespace Advent2018
 
-open Task2
-
 module Main =
-
     [<EntryPoint>]
     let main argv = 
-        let file = new System.IO.StreamReader("../../Tasks/input2.txt")
-        printfn "%A" <| task2and2 file
+        let file = new System.IO.StreamReader("../../Tasks/input3.txt")
+        printfn "%A" <| Task3.task3 file
         System.Console.ReadKey() |> ignore
         0 // return an integer exit code
---
+++ Task3.fs
@@ -0,0 +1,34 @@
+﻿namespace Advent2018
+
+open System.IO
+open System.Text.RegularExpressions
+
+module Task3 =
+    let pattern = @"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)"
+
+    type Claim = {id:int; left: int; top: int; width: int; height: int}
+
+    let getRequests(file: StreamReader) =
+        file.ReadToEnd().Split('\n')
+            |> Array.filter (fun str -> Regex.IsMatch(str, pattern))
+            |> Array.map(fun str -> 
+                let matches = Regex.Match(str, pattern).Groups
+                assert (matches.Count = 6)
+                {id = matches.[1].Value |> int; left = matches.[2].Value |> int; top = matches.[3].Value |> int; width = matches.[4].Value |> int; height = matches.[5].Value |> int})
+
+    let task3 (file: StreamReader) =
+        let reqs = getRequests file
+        let fabric = Array2D.zeroCreate 1000 1000
+
+        reqs
+        |> Array.iter (fun claim ->
+            for i in claim.left..(claim.left + claim.width) do
+                for j in claim.top..(claim.top + claim.height) do
+                    Array2D.set fabric i j (fabric.[i, j] + 1))
+
+        let mutable res = 0
+
+        fabric
+            |> Array2D.iter (fun x -> if x > 1 then res <- res + 1)
+
+        res
